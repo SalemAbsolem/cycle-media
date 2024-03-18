a734c9121bc73a3e097d926c43db8187345dfbcc
SalemAbsolem <smirnovalexander.cat@gmail.com>
2024-03-18 20:16:08 +0300

    feat: стилизует index для 480

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
-/* в этот файл добавляет скрипты*/
+const menuButton = document.querySelectorAll('.button--toogle-menu');
+const menuSite = document.querySelector('.header__menu');
+
+for(let i = 0; i < menuButton.length; i++) {
+  const menuText = menuButton[i].querySelector('.button__text');
+  menuButton[i].addEventListener('click', () => {
+    document.body.classList.toggle('body--stop-scrolling');
+    menuSite.classList.toggle('header__menu--is-open');
+    if(menuSite.classList.contains('header__menu--is-open')) {
+      menuText.textContent = 'закрыть';
+    } else {
+      menuText.textContent = 'меню?';
+    }
+  });
+}
